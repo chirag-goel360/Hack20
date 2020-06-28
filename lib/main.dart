66a7c3b65b513334350a8d4f33b26b0ca04c82a3
Ritik <ritikmiglani488@gmail.com>
2020-06-29 01:50:00 +0530

    chirag work

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'dart:convert';
+
 import 'package:farmerfame/Screens/CarbonFoot.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 import 'dart:math';
 import 'package:vector_math/vector_math.dart' show radians;
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
@@ -124,5 +128,167 @@ class RadialAnimation extends StatelessWidget {
             backgroundColor: color,
             onPressed: _close,
             elevation: 0));
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  var temp;
+  var description;
+  var humidity;
+  var wind;
+  var currently;
+  Future getData(double latitude, double longitude) async {
+    String api = 'http://api.openweathermap.org/data/2.5/weather';
+    String appId = 'db88d8a495a4e3fc4e181645da22796f';
+    String url = '$api?lat=$latitude&lon=$longitude&appid=$appId';
+    http.Response response = await http.get(url);
+    var result = json.decode(response.body);
+    setState(() {
+      this.temp = result['main']['temp'];
+      this.description = result['weather'][0]['description'];
+      this.humidity = result['main']['humidity'];
+      this.wind = result['wind']['speed'];
+      this.currently = result['weather'][0]['main'];
+    });
+  }
+
+  Future<Position> getPosition() async {
+    Position position = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    return position;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getPosition().then((position) {
+      getData(position.latitude, position.longitude);
+    });
+  }
+
+  TextEditingController _controller = new TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: MediaQuery.of(context).size.height / 3,
+            width: MediaQuery.of(context).size.width,
+            color: Colors.purpleAccent.shade100,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.grey,
+                              offset: Offset(1, 2),
+                              blurRadius: 10)
+                        ]),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.search,
+                        color: Colors.red,
+                      ),
+                      title: TextField(
+                        controller: _controller,
+                        decoration: InputDecoration(
+                            hintText: 'Enter new location',
+                            border: InputBorder.none),
+                        onChanged: (String value) {
+                          setState(() {});
+                        },
+                        onTap: () {
+                          setState(() {
+                            _controller.clear();
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 10),
+                  child: Text(
+                    "Temperature Details",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 14),
+                  ),
+                ),
+                Text(
+                  temp != null
+                      ? '${(temp - 273.15).toStringAsFixed(3)}°C'
+                      : "Loading",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40,
+                      fontWeight: FontWeight.w600),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10),
+                  child: Text(
+                    "current Temperature of Area",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 14),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(20),
+              child: ListView(
+                children: <Widget>[
+                  ListTile(
+                    leading: FaIcon(FontAwesomeIcons.thermometer),
+                    title: Text('Temperature'),
+                    trailing: Text(temp != null
+                        ? '${(temp - 273.15).toStringAsFixed(3)}°C'
+                        : "Loading"),
+                  ),
+                  ListTile(
+                    leading: FaIcon(FontAwesomeIcons.sun),
+                    title: Text('Humidity'),
+                    trailing: Text(humidity != null
+                        ? '${humidity.toString()}'
+                        : "Loading"),
+                  ),
+                  ListTile(
+                    leading: FaIcon(FontAwesomeIcons.cloud),
+                    title: Text('Weather'),
+                    trailing: Text(description != null
+                        ? '${description.toString()}'
+                        : "Loading"),
+                  ),
+                  ListTile(
+                    leading: FaIcon(FontAwesomeIcons.wind),
+                    title: Text('Wind'),
+                    trailing:
+                        Text(wind != null ? '${wind.toString()}' : "Loading"),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
