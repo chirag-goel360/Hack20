1fbe5339621c7d3eb31be8a41bca91c87077486b
Chirag Goel <chiggi03111998@gmail.com>
2020-06-28 20:15:10 +0530

    mergekarlena

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 import 'dart:math';
+import 'package:http/http.dart' as http;
 import 'package:vector_math/vector_math.dart' show radians;
 
 void main() {
@@ -122,6 +125,123 @@ class RadialAnimation extends StatelessWidget {
         ),
         child: FloatingActionButton(
             child: Icon(icon), backgroundColor: color, onPressed: _close, elevation: 0)
+    );
+  }
+}
+
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  var temp;
+  var description;
+  var humidity;
+  var wind;
+  var currently;
+  Future getData(double latitude, double longitude) async {
+    String api = 'http://api.openweathermap.org/data/2.5/weather';
+    String appId = 'db88d8a495a4e3fc4e181645da22796f';
+    String url = '$api?lat=$latitude&lon=$longitude&appid=$appId';
+    http.Response response = await http.get(url);
+    var result = json.decode(response.body);
+    setState(() {
+      this.temp = result['main']['temp'];
+      this.description = result['weather'][0]['description'];
+      this.humidity = result['main']['humidity'];
+      this.wind = result['wind']['speed'];
+      this.currently = result['weather'][0]['main'];
+    });
+  }
+  Future<Position> getPosition() async {
+    Position position = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    return position;
+  }
+  @override
+  void initState(){
+    super.initState();
+    getPosition().then((position) {
+      getData(position.latitude, position.longitude);
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: MediaQuery.of(context).size.height/3,
+            width: MediaQuery.of(context).size.width,
+            color: Colors.purpleAccent.shade100,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(bottom: 10),
+                  child: Text(
+                    "Temperature Details",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 14
+                    ),
+                  ),
+                ),
+                Text('${temp.toString()}°C',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontWeight: FontWeight.w600
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10),
+                  child: Text(
+                    "current Temperature of Area",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 14
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(20),
+              child: ListView(
+                children: <Widget>[
+                  ListTile(
+                    leading: FaIcon(FontAwesomeIcons.thermometer),
+                    title: Text('Temperature'),
+                    trailing: Text('${temp.toString()}°C'),
+                  ),
+                  ListTile(
+                    leading: FaIcon(FontAwesomeIcons.sun),
+                    title: Text('Humidity'),
+                    trailing: Text('${humidity.toString()}'),
+                  ),ListTile(
+                    leading: FaIcon(FontAwesomeIcons.cloud),
+                    title: Text('Weather'),
+                    trailing: Text('${description.toString()}'),
+                  ),
+                  ListTile(
+                    leading: FaIcon(FontAwesomeIcons.wind),
+                    title: Text('Wind'),
+                    trailing: Text('${wind.toString()}'),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
