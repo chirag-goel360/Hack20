6ffecdfee619d8553e8044a9b7d530f390ff7328
skullcandy <C@nstrue1>
2020-06-29 16:39:08 +0530

    final update

--- lib/Screens/CarbonResult.dart
+++ lib/Screens/CarbonResult.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:mccounting_text/mccounting_text.dart';
 
 class CarbonResult extends StatelessWidget {
   final res;
@@ -6,38 +8,74 @@ class CarbonResult extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.green,
       ),
+      backgroundColor: Colors.green,
       body: Container(
-        height: size.height,
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             Container(
-              height: size.height * 0.25,
-              width: size.width,
-              color: Colors.green[300],
+              // height: size.height * 0.5,
+              // width: size.width,
+              color: Colors.green,
               child: Column(
-                // mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                        'Current Carbon FootPrint of your household is ${res.toStringAsFixed(1)}',
+                  // Padding(
+                  //   padding: const EdgeInsets.all(8.0),
+                  //   child: Text(
+                  //       'Current Carbon FootPrint of your household is ${res.toStringAsFixed(1)}',
+                  //       style: TextStyle(color: Colors.white, fontSize: 30)),
+                  // ),
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    child: Text("Current Carbon Footprint of your household is",
+                        textAlign: TextAlign.center,
                         style: TextStyle(color: Colors.white, fontSize: 30)),
                   ),
+                  McCountingText(
+                    begin: 0,
+                    end: res.toDouble(),
+                    style: Theme.of(context).textTheme.headline3,
+                    duration: Duration(seconds: 2),
+                    curve: Curves.decelerate,
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    child: Text("Tons",
+                        style: TextStyle(color: Colors.white, fontSize: 30)),
+                  ),
+
+                  res < 12
+                      ? SizedBox(
+                          width: 400.0,
+                          child: ColorizeAnimatedTextKit(
+                              speed: Duration(milliseconds: 100),
+                              onTap: () {
+                                print("Tap Event");
+                              },
+                              text: [
+                                "Congratulations!",
+                                "You are doing better than others!",
+                              ],
+                              textStyle: TextStyle(fontSize: 36.0),
+                              colors: [
+                                Colors.white,
+                                Colors.black,
+                              ],
+                              textAlign: TextAlign.center,
+                              alignment: AlignmentDirectional
+                                  .topStart // or Alignment.topLeft
+                              ),
+                        )
+                      : Container()
                 ],
               ),
             ),
-            res < 12
-                ? Container(
-                  color: Colors.white,
-                    height: size.height * 0.6,
-                    child: Image.asset('images/hurray.gif'),
-                  )
-                : Container(),
           ],
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,131 +1,44 @@
 import 'package:farmerfame/Screens/CarbonFoot.dart';
+import 'package:farmerfame/Weather.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'dart:math';
 import 'package:vector_math/vector_math.dart' show radians;
 
+import 'Screens/comparison.dart';
+
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget { 
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: CarbonFootPrint(),
-    debugShowCheckedModeBanner: false,
+    return MaterialApp(
+      home: Stats(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
+class CircularButton extends StatelessWidget {
 
+  final double width;
+  final double height;
+  final Color color;
+  final Icon icon;
+  final Function onClick;
 
-class RadialMenu extends StatefulWidget {
-  createState() => _RadialMenuState();
-}
+  CircularButton({this.color, this.width, this.height, this.icon, this.onClick});
 
-class _RadialMenuState extends State<RadialMenu>
-    with SingleTickerProviderStateMixin {
-  AnimationController controller;
-  @override
-  void initState() {
-    super.initState();
-    controller =
-        AnimationController(duration: Duration(milliseconds: 900), vsync: this);
-  }
 
   @override
   Widget build(BuildContext context) {
-    return RadialAnimation(controller: controller);
+    return Container(
+      decoration: BoxDecoration(color: color,shape: BoxShape.circle),
+      width: width,
+      height: height,
+      child: IconButton(icon: icon,enableFeedback: true, onPressed: onClick),
+    );
   }
 }
-
-class RadialAnimation extends StatelessWidget {
-  RadialAnimation({Key key, this.controller})
-      : translation = Tween<double>(
-          begin: 0.0,
-          end: 100.0,
-        ).animate(
-          CurvedAnimation(parent: controller, curve: Curves.linear),
-        ),
-        scale = Tween<double>(
-          begin: 1.5,
-          end: 0.0,
-        ).animate(
-          CurvedAnimation(parent: controller, curve: Curves.fastOutSlowIn),
-        ),
-        rotation = Tween<double>(
-          begin: 0.0,
-          end: 360.0,
-        ).animate(
-          CurvedAnimation(
-            parent: controller,
-            curve: Interval(
-              0.3,
-              0.9,
-              curve: Curves.decelerate,
-            ),
-          ),
-        ),
-        super(key: key);
-  final AnimationController controller;
-  final Animation<double> scale;
-  final Animation<double> rotation;
-  final Animation<double> translation;
-  build(context) {
-    return AnimatedBuilder(
-        animation: controller,
-        builder: (context, builder) {
-          return Transform.rotate(
-            angle: radians(rotation.value),
-            child: Stack(alignment: Alignment.center, children: [
-              _buildButton(0, color: Colors.red, icon: FontAwesomeIcons.sun),
-              _buildButton(60,
-                  color: Colors.green, icon: FontAwesomeIcons.sprayCan),
-              _buildButton(120,
-                  color: Colors.orange, icon: FontAwesomeIcons.fire),
-              _buildButton(180,
-                  color: Colors.blue, icon: FontAwesomeIcons.kiwiBird),
-              _buildButton(240,
-                  color: Colors.black, icon: FontAwesomeIcons.cat),
-              _buildButton(300,
-                  color: Colors.indigo, icon: FontAwesomeIcons.paw),
-              Transform.scale(
-                scale: scale.value - 1.5,
-                child: FloatingActionButton(
-                    child: Icon(FontAwesomeIcons.timesCircle),
-                    onPressed: _close,
-                    backgroundColor: Colors.red),
-              ),
-              Transform.scale(
-                scale: scale.value,
-                child: FloatingActionButton(
-                    child: Icon(FontAwesomeIcons.solidDotCircle),
-                    onPressed: _open),
-              )
-            ]),
-          );
-        });
-  }
-
-  _open() {
-    controller.forward();
-  }
-
-  _close() {
-    controller.reverse();
-  }
-
-  _buildButton(double angle, {Color color, IconData icon}) {
-    final double rad = radians(angle);
-    return Transform(
-        transform: Matrix4.identity()
-          ..translate(
-              (translation.value) * cos(rad), (translation.value) * sin(rad)),
-        child: FloatingActionButton(
-            child: Icon(icon),
-            backgroundColor: color,
-            onPressed: _close,
-            elevation: 0));
-  }
-}
-
